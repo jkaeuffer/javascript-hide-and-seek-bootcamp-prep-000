bf995bf3f6cd6ad8e6fa1b5302d0bef211081cc8
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-13 22:52:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function deepestChild() {
 function increaseRankBy (n) {
   let ranked = document.querySelector('.ranked-list')
   let bullets = ranked.querySelectorAll('li')
-  bullets.forEach(function(e)) {
+  bullets.forEach(function(e) {
     let i = e.innerText
     let iNumber = parseInt(i,10)
     iNumber += 1
     e.innerText = iNumber
-  }
+  })
 
 }
