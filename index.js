3a6f254fff5154d684323df8e51d6c75dba3666e
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-13 22:28:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function getFirstSelector(selector) {
-  
+  let found = document.querySelector(selector)
+  return found
 }
 
 function nestedTarget() {
-  
+
 }
 
 function deepestChild() {
-  
+
 }
 function increaseRankBy (n) {
-  
+
 }
